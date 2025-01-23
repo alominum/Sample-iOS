295a9981820123d2650b2d7f93aab584bc66b19f
Nima Nassehi <n.nassehi@intellisports.net>
2025-01-23 15:09:02 -0500

    Add UIComposer and load the MainVC programatically

---
+++ OneSpan/Composers/FeedUIComposer.swift
@@ -0,0 +1,25 @@
+//
+//  FeedUIComposer.swift
+//  OneSpan
+//
+//  Created by Nima Nassehi on 2025-01-23.
+//
+
+import UIKit
+
+final class UIComposer {
+    private init() {}
+
+    static func composedVC() -> MainViewController {
+        let controller = makeMainViewController(title: "Dog Breeds")
+        return controller
+    }
+
+    private static func makeMainViewController(title: String) -> MainViewController {
+        let bundle = Bundle(for: MainViewController.self)
+        let storyboard = UIStoryboard(name: "Main", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! MainViewController
+        controller.title = title
+        return controller
+    }
+}
