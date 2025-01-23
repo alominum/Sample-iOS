c22b7527e09ee66647f65d58c4b7a65e3224cafc
Nima Nassehi <n.nassehi@intellisports.net>
2025-01-23 11:48:53 -0500

    Add dog table cell class

---
+++ OneSpan/Views/DogTableCell.swift
@@ -0,0 +1,16 @@
+//
+//  TableCell.swift
+//  OneSpan
+//
+//  Created by Nima Nassehi on 2025-01-23.
+//
+
+import UIKit
+
+class DogTableCell: UITableViewCell {
+
+    @IBOutlet private(set) public var breedName: UILabel!
+    @IBOutlet private(set) public var dogImageView: UIImageView!
+
+
+}
