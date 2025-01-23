b7bc1e30cd67401c16aadf5e812fa7d9d666e5eb
Nima Nassehi <n.nassehi@intellisports.net>
2025-01-23 12:19:33 -0500

    Add TableViewCellViewModel

---
+++ OneSpan/ViewModels/TableCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewModel.swift
+//  OneSpan
+//
+//  Created by Nima Nassehi on 2025-01-24.
+//
+
+import UIKit
+
+struct TableCellViewModel {
+    let name: String
+    let image: UIImage
+}
