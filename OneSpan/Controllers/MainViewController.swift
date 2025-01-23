162605d6f6e42f6f040d0864f927a3631470fe90
Nima Nassehi <n.nassehi@intellisports.net>
2025-01-23 15:27:26 -0500

    Add VC loading Indicator

--- OneSpan/Controllers/MainViewController.swift
+++ OneSpan/Controllers/MainViewController.swift
@@ -14,9 +14,10 @@ protocol ViewControllerDelegate {
 class MainViewController: UIViewController {
     var delegate: ViewControllerDelegate?
 
-    @IBOutlet var tableView: UITableView!
-    @IBOutlet var refreshButtonContainer: UIView!
-    @IBOutlet var refreshButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var refreshButtonContainer: UIView!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +25,12 @@ class MainViewController: UIViewController {
         tableView.dataSource = self
 
         setupUI()
+        refresh()
     }
 
     private func setupUI() {
         refreshButtonContainer.addShaddow()
+        loadingIndicator.hidesWhenStopped = true
     }
 
     @IBAction func refresh() {
