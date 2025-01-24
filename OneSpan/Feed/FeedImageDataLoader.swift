b0a615992871ed09ff74c9ebc61efb097cf5c690
Nima Nassehi <n.nassehi@intellisports.net>
2025-01-23 21:14:04 -0500

    Add FeedImageDataLoader

---
+++ OneSpan/Feed/FeedImageDataLoader.swift
@@ -0,0 +1,18 @@
+//
+//  DogLoader.swift
+//  OneSpan
+//
+//  Created by Nima Nassehi on 2025-01-24.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
